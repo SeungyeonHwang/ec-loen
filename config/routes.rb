e8ca7ec53d1f6aece3b61761c57f3bc7f74f3bc1
seungyeon.hwang <syhwang.web@gmail.com>
2020-11-05 22:48:40 +0900

    #Update - Create User Model - Product default Image Changed - Navbar into shared - routes update -> RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'cosmetics/index'
-  get 'cosmetics/show/:id' => "cosmetics#show"
 
-  root 'home#index'
+  resources :cosmetics, only: [:index, :show]
+
+  # get "cosmetics" => "cosmetics#index"
+  # get "cosmetics/:id" => "cosmetics/show"
+
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
