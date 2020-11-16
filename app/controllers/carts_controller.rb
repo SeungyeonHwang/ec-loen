a9efbc779f01f522e5f25aa55d7dfe7534e92cd9
HwangSeungyeon <syhwang.web@gmail.com>
2020-11-16 22:23:44 +0900

    #Cart - カート入れる時のメッセージ - コスメ情報とカートの連携 - カートの削除機能

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,17 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!
 
+  # カート情報の連携
   def index
     @carts = current_user.carts
   end
 
+  ##TODO: 既存のカートに同一商品があるか判断する処理　１６：４１
+  user_carts = current_user.carts
+
+  origin_cart = user_carts.find_by(cosmetic_id: params[:cosmetic_id])
+
+  # 商品をカートに入れるコード
   def create
     Cart.create(
       cosmetic_id: params[:cosmetic_id],
@@ -12,6 +19,16 @@ class CartsController < ApplicationController
       quantity: params[:quantity],
     )
 
+    # redirect_backで使えるAlert。（Create成功を前提にする）
+    flash[:notice] = "カートに追加されました。レジに進みますか。"
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    cart = Cart.find(params[:id])
+
+    cart.destroy
     redirect_back(fallback_location: root_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # get "cosmetics/:id" => "cosmetics/show"
   resources :cosmetics, only: [:index, :show]
 
-  resources :carts, only: [:create, :index]
+  resources :carts, only: [:create, :index, :destroy]
 
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
