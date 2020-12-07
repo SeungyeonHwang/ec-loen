2f383bcb6bbb2c6cf28ac8dc66aa05b2db15f572
HwangSeungyeon <syhwang.web@gmail.com>
2020-12-07 23:09:28 +0900

    ref #2 - ホームデザイン修正 - ビューティー記事Showページ作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,12 +2,11 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.order("created_at DESC").all
     @tags = Blog.all.select(:tag).distinct
-
     if params[:tag].present?
       @blogs = Blog.where(tag: params[:tag]).order("created_at DESC").all
     end
-
   end
+  
   def show
     @blogs = Blog.find(params[:id])
   end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,12 @@ class Blog < ApplicationRecord
   has_one_attached :image
 
   validates :title, :image, :tag, :content, presence: true
+
+  def next
+    Blog.where("id > ?", id).first
+  end
+
+  def prev
+    Blog.where("id < ?", id).last
+  end
 end
