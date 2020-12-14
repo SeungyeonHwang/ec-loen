691225b7e8247837328494846e4d9034930f4db2
HwangSeungyeon <syhwang.web@gmail.com>
2020-12-14 19:03:10 +0900

    fix

--- db/migrate/20201214095802_change_rating_to_be_integer_in_cosmetics_try.rb
+++ db/migrate/20201214095802_change_rating_to_be_integer_in_cosmetics_try.rb
@@ -1,5 +1,5 @@
 class ChangeRatingToBeIntegerInCosmeticsTry < ActiveRecord::Migration[5.2]
   def change
-    change_column :cosmetics, :rating, 'integer USING CAST(column_name AS integer)'
+    change_column :cosmetics, :rating, 'integer USING CAST(rating AS integer)'
   end
 end
