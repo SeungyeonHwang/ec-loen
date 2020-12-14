42942c139bd10885b39fdc87f99626b52a89dbd7
HwangSeungyeon <syhwang.web@gmail.com>
2020-12-14 22:49:22 +0900

    no message

--- db/migrate/20201214094431_change_rating_to_be_integer_in_cosmetics.rb
+++ db/migrate/20201214094431_change_rating_to_be_integer_in_cosmetics.rb
@@ -1,6 +1,6 @@
 class ChangeRatingToBeIntegerInCosmetics < ActiveRecord::Migration[5.2]
   def up
-    change_column :cosmetics, :rating, 'integer USING CAST(rating AS integer)'
+    # change_column :cosmetics, :rating, 'integer USING CAST(rating AS integer)'
   end
 end
 
