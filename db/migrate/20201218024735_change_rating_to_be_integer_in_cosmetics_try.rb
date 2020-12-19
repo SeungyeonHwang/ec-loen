a63588e535fee8b6c4fb8524465b405ee11060fd
HwangSeungyeon <syhwang.web@gmail.com>
2020-12-20 06:26:52 +0900

    update

--- db/migrate/20201218024735_change_rating_to_be_integer_in_cosmetics_try.rb
+++ db/migrate/20201218024735_change_rating_to_be_integer_in_cosmetics_try.rb
@@ -1,5 +1,5 @@
 class ChangeRatingToBeIntegerInCosmeticsTry < ActiveRecord::Migration[5.2]
   def change
-      # change_column :cosmetics, :rating, :integer
+      change_column :cosmetics, :rating, :integer
   end
 end
